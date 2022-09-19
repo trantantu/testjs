f9d412fd72d05a4268c086ba1cdc261384a2fc39
trantantu <trantantu85@gmail.com>
2022-09-19 13:59:09 +0700

    Cai dat du an

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app = require("./app");
 const config = require("./app/config");
 
 const port = config.app.port;
-app.listen(PORT, () => {
-    console.log('Server is running at port ${PORT}.');
+app.listen(port, () => {
+    console.log('Server is running at port ${port}.');
 });
